72b30010d530159fa5619acea1d9c9f1612d319e
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-06-12 10:15:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob' ;
+
+function upperCaseCustomerName() {
+	customerName = customerName.toUpperCase()
+} 
+
+function setBestCustomer() {
+	bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(customer) {
+	// body... 
+	return bestCustomer = customer;
+}
+
+const leastFavoriteCustomer = 'Maya';
+
+function changeLeastFavoriteCustomer(customer) {
+	// body...
+	leastFavoriteCustomer = customer;
+}
